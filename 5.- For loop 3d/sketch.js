47bd9f571073cde81f5373e14d33c5965904b999
catalinariverost <54757746+catalinariverost@users.noreply.github.com>
2019-12-20 21:39:25 -0300

    Add files via upload

---
+++ 5.- For loop 3d/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(900, 900, WEBGL);
+}
+
+function draw() {
+  background(0);
+  
+// rotatex(frameCount * 0.01);
+  rotateY(frameCount * 100);
+  {ellipsoid(mouseX,mouseY)
+  
+  fill('rgba(100%,0%,100%,0.5)')
+}
+  if (mouseIsPressed){
+   fill('rgba(0,255,0, 0.25)');
+  sphere(50)}
+  
+  strokeWeight(1);
+  box(60,60,60);
+  fill(90)
+  
+
+}
+  
+  
+  
